e225d7e8d140df43b6a7549b2c6c3dc81893c08a
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-01-15 19:22:41 -0300

    feat: add some methods on wave contract + interact with it via script

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,12 +1,24 @@
 import { ethers } from "hardhat"
 
 async function main() {
+  const [owner, randomPerson] = await ethers.getSigners()
   const waveContractFactory = await ethers.getContractFactory("WavePortal")
   const waveContract = await waveContractFactory.deploy()
 
   await waveContract.deployed()
 
   console.log("Contract deployed to:", waveContract.address)
+  console.log("Contract deployed by:", owner.address)
+
+  await waveContract.getTotalWaves()
+
+  const waveTxn = await waveContract.wave()
+  await waveTxn.wait()
+
+  const randomWaveTx = await waveContract.connect(randomPerson).wave()
+  await randomWaveTx.wait()
+
+  await waveContract.getTotalWaves()
 }
 
 const runMain = async () => {
