e7992b8f536fb823f518b04433b8c67be60cda2d
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-02-09 23:22:14 -0300

    feat(indexing): add ilustrative console logs while getting transfers

--- app/routes/projects/indexing.tsx
+++ app/routes/projects/indexing.tsx
@@ -7,58 +7,52 @@ const COMP_ADDRESS = "0xc00e94cb662c3520282e6f5717214004a7f26888"
 
 export default function IndexingProject(): ReactElement {
   const [transferEvents, setTransferEvents] = useState<TransferEvent[]>([])
+  console.log("IndexingProject ~ transferEvents", transferEvents)
 
   useEffect(() => {
-    async function indexTranferEvents() {
-      const startBlock = 9601359
-      const endBlock = 9603359
+    const startBlock = 9601359
+    const endBlock = 9605359
 
-      const chainId = ChainId.Mainnet
-      const address = COMP_ADDRESS
-      const erc20Contract = getErc20Contract({ address, chainId })
+    const chainId = ChainId.Mainnet
+    const address = COMP_ADDRESS
+    const erc20Contract = getErc20Contract({ address, chainId })
 
-      async function getTransferEvents(
-        startBlock: number,
-        endBlock: number,
-        erc20Contract: Erc20,
-      ): Promise<TransferEvent[]> {
-        const transferEvents: TransferEvent[] = []
+    async function getTransferEvents(
+      startBlock: number,
+      endBlock: number,
+      erc20Contract: Erc20,
+    ): Promise<void> {
+      const BATCH_SIZE = 100
 
-        const BATCH_SIZE = 100
+      for (
+        let fromBlock = startBlock;
+        fromBlock <= endBlock;
+        fromBlock += BATCH_SIZE
+      ) {
+        const toBlock = Math.min(fromBlock + BATCH_SIZE - 1, endBlock)
+        const transfersFilter = erc20Contract.filters.Transfer()
 
-        for (
-          let fromBlock = startBlock;
-          fromBlock <= endBlock;
-          fromBlock += BATCH_SIZE
-        ) {
-          const toBlock = Math.min(fromBlock + BATCH_SIZE - 1, endBlock)
-          const transfersFilter = erc20Contract.filters.Transfer()
+        const nextTransferEvents = await erc20Contract.queryFilter(
+          transfersFilter,
+          fromBlock,
+          toBlock,
+        )
 
-          const transferEvents = await erc20Contract.queryFilter(
-            transfersFilter,
-            fromBlock,
-            toBlock,
-          )
+        console.log("indexTranferEvents ~ fromBlock", fromBlock)
+        console.log("indexTranferEvents ~ toBlock", toBlock)
+        console.log(
+          "indexTranferEvents ~ nextTransferEvents ",
+          nextTransferEvents,
+        )
 
-          setTransferEvents((prevTransferEvents) => [
-            ...prevTransferEvents,
-            ...transferEvents,
-          ])
-        }
-
-        return transferEvents
+        setTransferEvents((prevTransferEvents) => [
+          ...prevTransferEvents,
+          ...nextTransferEvents,
+        ])
       }
-
-      const transferEvents: TransferEvent[] = await getTransferEvents(
-        startBlock,
-        endBlock,
-        erc20Contract,
-      )
-
-      setTransferEvents(transferEvents)
     }
 
-    indexTranferEvents()
+    getTransferEvents(startBlock, endBlock, erc20Contract)
   }, [])
 
   return (
